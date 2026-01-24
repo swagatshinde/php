347566bbbac8e9d4652c313792787d3b4c3ae86f
Swagat Shinde <htcswagatshinde@gmail.com>
2026-01-24 17:21:07 +0530

    Add files via upload

---
+++ cookie.php
@@ -0,0 +1,16 @@
+<?php
+setcookie("username", "iamNotswagat", time() + 60 * 60 * 24 * 7);
+?>
+<html>
+
+<body>
+    <?php
+    if (isset($_COOKIE["username"])) {
+        echo "Cookie set with value :" . $_COOKIE["username"];
+    } else {
+        echo "cookie not sent!";
+    }
+    ?>
+</body>
+
+</html>
